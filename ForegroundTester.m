06bca43a46ad833ac8ef0af16b04300b8b303010
John Moosemiller <itcaughtonj@gmail.com>
2018-09-01 23:38:43 -0500

    Update ForegroundTester.m

--- ForegroundTester.m
+++ ForegroundTester.m
@@ -1,17 +1,24 @@
 %Foreground Tester
 clc
 clear
-close
-
-Sigma = 2
+close all
+Frame=[1]
+Sigma = 8
 FrameCount = 1
 SD=[]
 Composite=[]
-NumTrainingFrames=10
+NumTrainingFrames=20
 figure(1)
 for F = 1:256
-  Frame = randi([0,255],3,4);
-  [ForegroundMask,Composite,SD,FrameCount] = ForegroundDetector (Frame, Sigma, Composite, SD, FrameCount, NumTrainingFrames);
+  FrameP=Frame;
+  Frame = aviread('SC.avi',60*F);
+  FrameRed = Frame(:,:,1);
+  [ForegroundMask,Composite,SD,FrameCount] = ForegroundDetector (FrameRed, Sigma, Composite, SD, FrameCount, NumTrainingFrames);
+  figure(1)
   imshow(ForegroundMask);
-  pause(.5)
+  figure(2)
+  imshow(FrameRed)
+  NumDiffs = sum(sum(ForegroundMask))
+  pause(2)
+  FrameSame = sum(sum(sum(FrameP~=Frame)))
 endfor
