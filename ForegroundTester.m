eee7cab5a4dab08d88d9cf25c41abb21b8717c21
John Moosemiller <itcaughtonj@gmail.com>
2018-09-02 19:33:36 -0500

    Update ForegroundTester.m

--- ForegroundTester.m
+++ ForegroundTester.m
@@ -1,24 +1,24 @@
-%Foreground Tester
 clc
 clear
-close all
-Frame=[1]
-Sigma = 8
+close
+
+Sigma = 1 % the higher this is set the fewer pixels will pass.
 FrameCount = 1
 SD=[]
 Composite=[]
-NumTrainingFrames=20
+NumTrainingFrames=10 %arbitrary, the higher this is the more data will contribute to your normal distribution and you'll get more predictable results
 figure(1)
-for F = 1:256
-  FrameP=Frame;
-  Frame = aviread('SC.avi',60*F);
-  FrameRed = Frame(:,:,1);
-  [ForegroundMask,Composite,SD,FrameCount] = ForegroundDetector (FrameRed, Sigma, Composite, SD, FrameCount, NumTrainingFrames);
-  figure(1)
+for F = 1:256 % number of frames is arbitrary, just has to be > NumTrainingFrames
+  
+  tic
+  Frame = randi([0,255],1080,1920);
+  FrameGenTime = toc
+  tic
+  [ForegroundMask,Composite,SD,FrameCount] = ForegroundDetector (Frame, Sigma, Composite, SD, FrameCount, NumTrainingFrames);
+  ForegroundDetectTime = toc                                          
+
   imshow(ForegroundMask);
-  figure(2)
-  imshow(FrameRed)
-  NumDiffs = sum(sum(ForegroundMask))
-  pause(2)
-  FrameSame = sum(sum(sum(FrameP~=Frame)))
+  pause(.1);
+
+
 endfor
